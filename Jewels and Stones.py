9396afc890d6b7aa6abfb392af032db8cd7451e6
Gregory Hogg <gahogg@uwaterloo.ca>
2020-07-02 12:00:56 -0400

    Update Jewels and Stones.py

--- Jewels and Stones.py
+++ Jewels and Stones.py
@@ -1,9 +1,13 @@
 class Solution:
     def numJewelsInStones(self, J: str, S: str) -> int:
         count = 0
+        d = {}
+        
+        for jewel in J:
+            d[jewel] = 1
         
         for s in S:
-            if s in J:
+            if s in d:
                 count += 1
         
         return count
