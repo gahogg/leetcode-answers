0839428eca472d5e424421f619e1e86be527bcc0
Gregory Hogg <gahogg@uwaterloo.ca>
2020-07-21 15:20:13 -0400

    Create DI String Match.py

---
+++ DI String Match.py
@@ -0,0 +1,16 @@
+from collections import deque
+
+
+class Solution:
+    #  O(n) runtime, O(n) space
+    def diStringMatch(self, S: str) -> List[int]:
+        N = len(S)
+        deque_0_to_N = deque(list(range(N + 1)))
+        sol_so_far = []
+        for letter in S:
+            if letter == "I":
+                sol_so_far.append(deque_0_to_N.popleft())
+            else:
+                sol_so_far.append(deque_0_to_N.pop())
+        sol_so_far.append(deque_0_to_N.pop())
+        return sol_so_far
